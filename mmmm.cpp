c7abf96ba353e775a6f34980d2f269f457cf6095
zxcown <115359959+zxcown@users.noreply.github.com>
2022-10-22 19:29:37 +0300

    Add files via upload

---
+++ mmmm.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//Series1◦
+	/*double x,  sum = 0;
+	int i = 1;
+	while (i <= 10) {
+		cin >> x;
+		sum += x;
+		i++;
+	}
+	cout << sum ;*/
+	//Series2
+	/*int x, i , pro = 1;
+	for (i = 1; i <= 10; i ++) {
+		cin >> x;
+		pro *= x;
+	}
+	cout << pro;*/
+	//Series3
+	/*double x, sum = 0;
+	int i = 1;
+	double aref = 0;
+	while (i <= 10) {
+		cin >> x;
+		sum += x;
+		aref = sum / 10;
+		i++;
+	}
+	cout << aref;*/
+	//Series4
+	/*int i ;
+	double n;
+	double sum = 0, pro = 1;
+	int x;
+	cin >> n;
+	for (i = 1; i <= n; i++) {
+		cin >> x;
+		sum += x;
+		pro *= x;
+	}
+	cout << sum << " ";
+	cout << pro << " ";*/
+	//Series5
+	int N;
+	int i = 1, sum = 1;
+	double n;
+	cin >> N;
+	while (i <= N) {
+		cin >> n;
+		sum +=  n;
+		cout << n<< "  ";
+		i++;
+	}
+	cout << sum ;
+	//Series6
+	//Series7
+}
